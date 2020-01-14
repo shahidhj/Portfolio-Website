c9743e747c3539bf5c44c90fc3f675e7328a8642
shahidhj <shahidhj@gmail.com>
2020-01-13 18:05:08 -0800

    setting up database as csv

--- server.py
+++ server.py
@@ -1,9 +1,20 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for,request, redirect
+import csv
 app = Flask(__name__)
 
 @app.route('/')
 def homePage():
     return render_template("index.html")
+
+
+def write_to_csv(data):
+    with open('database.csv',mode='a',newline='') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csvWrite = csv.writer(database,delimiter=",",quotechar='"',quoting=csv.QUOTE_MINIMAL)
+        csvWrite.writerow([email,subject,message])
+
 
 @app.route('/<string:page_name>')
 def html_page(page_name):
@@ -11,6 +22,11 @@ def html_page(page_name):
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_page():
+    if request.method == "POST":
+        data = request.form.to_dict()
+        write_to_csv(data)
+        print(data)
+        return redirect("/thankyou.html")
 
     return 'form submitted'
 
